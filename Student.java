eb36e072598924f8cbfedfb4272d7ff0240a3b7e
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 17:03:46 +0530

    Create Student class

--- Student.java
+++ Student.java
@@ -1,1 +1,14 @@
-
+// Student.java
+import java.time.LocalDate;
+public class Student {
+    private String prn;
+    private String name;
+    private LocalDate dob;
+    private double marks;
+    public Student(String prn, String name, LocalDate dob, double marks) {
+        this.prn = prn;
+        this.name = name;
+        this.dob = dob;
+        this.marks = marks;
+    }
+}
