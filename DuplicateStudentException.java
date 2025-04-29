31923623020d2445ae2c189aaf1f14a510039475
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 17:00:56 +0530

    Create Duplicate Student Exception using custom exception class

--- DuplicateStudentException.java
+++ DuplicateStudentException.java
@@ -1,1 +1,6 @@
-
+// DuplicateStudentException.java
+public class DuplicateStudentException extends CustomException {
+    public DuplicateStudentException(String msg) {
+        super(msg);
+    }
+}
